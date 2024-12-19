f598c53e2440fba6f1749a7595975ea2096f0922
Mlemlus <mamlasmine@gmail.com>
2024-12-19 20:36:14 +0100

    Update dockerfile, create roles script

--- gtnh-docker/postgres/04_create_roles.sh
+++ gtnh-docker/postgres/04_create_roles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run a sql command to create the role with password enviroment variable 
-psql -U postgres -d postgres -c "CREATE ROLE gtoverseer_app WITH LOGIN ENCRYPTED PASSWORD '${POSTGRES_PASSWORD}'"
+psql -U "${POSTGRES_USER}" -d postgres -c "CREATE ROLE gtoverseer_app WITH LOGIN ENCRYPTED PASSWORD '${GTOVERSEER_POSTGRES_PASSWORD}'"
