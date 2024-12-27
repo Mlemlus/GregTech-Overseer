6ec80bec965a8e8e5c28dc00fb393825bf8fb3b1
Mlemlus <mamlasmine@gmail.com>
2024-12-27 16:22:07 +0100

    Update insert query for coord, power_source

--- gtnh-docker/gtoverseer-backend/data/data_process/data_processes.py
+++ gtnh-docker/gtoverseer-backend/data/data_process/data_processes.py
@@ -1,5 +1,5 @@
 import data.database.query as q
-import sys ##########
+import sys
 
 def insRestart(db, data): # Handling of data for the OC reinitialization
     # convert table tier to dictionary
@@ -21,7 +21,6 @@ def insRestart(db, data): # Handling of data for the OC reinitialization
         data[i]["oc_computer_ID"] = computer_oc_ID
         try:
             q.insMachine(db, data[i])
-            data[i]["machine_ID"] = q.selMachine(db, data[i])[0]
             q.insCoord(db, data[i])
         except Exception as e: # we keep this on the hush hush
             print(f"data_processes.insRestart: Failed to insert machine {data[i]["name"]}: {e}", file=sys.stderr)
